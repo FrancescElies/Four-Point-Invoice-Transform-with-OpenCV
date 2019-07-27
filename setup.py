e385200682543a0338b4f5609e88b01c58209b37
Francesc Elies Henar <francesc.elies@gmail.com>
2019-07-27 12:52:15 +0200

    version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name=package,
-    version="0.0.3",
+    version="0.0.4",
     install_requires=[
         "opencv-python-headless",
         "numpy",
