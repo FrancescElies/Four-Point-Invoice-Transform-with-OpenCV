c533e619e50584aab1540b0438113fb753a2179d
Francesc Elies Henar <francesc.elies@gmail.com>
2021-01-03 18:21:45 +0100

    adds typer dependency

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     install_requires=[
         "opencv-python-headless",
         "numpy",
-        "docopt",
+        "typer[all]",
         "schema",
         "setuptools",
         # "imutils",
